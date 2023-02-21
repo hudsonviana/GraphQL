8aeb0af116e082233ff54103b8803f2bfbc211c4
hudsonviana <hudson1206@gmail.com>
2023-02-21 10:51:23 -0300

    Como criar Types, Queries e Mutations no GraphQL - Aula #02

--- src/index.js
+++ src/index.js
@@ -1,14 +1,73 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    author: User!
+  }
+
   type Query {
     hello: String
+    users: [User]!
+    getUserByEmail(email: String!): User!
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
   }
 `;
+
+const users = [
+  {
+    _id: String(Math.random()),
+    name: 'Hudson',
+    email: 'hudson@teste.com',
+    active: true,
+  },
+  {
+    _id: String(Math.random()),
+    name: 'Hudson 2',
+    email: 'hudson2@teste.com',
+    active: false,
+  },
+  {
+    _id: String(Math.random()),
+    name: 'Hudson 3',
+    email: 'hudson3@teste.com',
+    active: true,
+  },
+];
 
 const resolvers = {
   Query: {
     hello: () => 'Hello, World!',
+    users: () => users,
+    getUserByEmail: (_, args) => {
+      return users.find((user) => user.email === args.email);
+    },
+  },
+
+  Mutation: {
+    createUser: (_, args) => {
+      const newUser = {
+        _id: String(Math.random()),
+        name: args.name,
+        email: args.email,
+        active: true,
+      };
+
+      users.push(newUser);
+      return newUser;
+    },
   },
 };
 
